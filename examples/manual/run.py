6585fae730441c536f746f2b2de356cb6fca5358
Adam Biśta <410788@student.agh.edu.pl>
2026-03-17 10:33:14 +0100

    add license

--- examples/manual/run.py
+++ examples/manual/run.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+
 from signfetch import download_data
+
 
 result = download_data(
     "https://doi.org/10.5281/zenodo.4318838",
-    output_dir=Path("downloads")
+    output_dir=Path("downloads"),
 )
 print(result)
